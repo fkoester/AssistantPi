3a3345139bdfb158e9d795321b47bd691b47c034
Martin Kainzbauer <m.kainzbauer@xtools.at>
2017-06-14 14:46:33 +0200

    resume playback after volume changing voice command

--- src/main.py
+++ src/main.py
@@ -123,6 +123,10 @@ class Player(object):
     playlist_last_item = None
     progressReportRequired = []
 
+    ###
+    last_playlist = None
+    ###
+
     def __init__(self, config, platform, pHandler): # pylint: disable=redefined-outer-name
         self.config = config
         self.platform = platform
@@ -130,6 +134,9 @@ class Player(object):
         self.tunein_parser = tunein.TuneIn(5000)
 
     def play_playlist(self, payload):
+        ###
+        self.last_playlist = payload
+        ###
         self.navigation_token = payload['navigationToken']
         self.playlist_last_item = payload['audioItem']['streams'][-1]['streamId']
 
@@ -341,10 +348,11 @@ def assistant_handler(voice_command):
         return False
     else:
         voice_command = voice_command.rstrip()
-    logger.debug('Pocketsphinx triggered with hotword: **' + voice_command + '**')
+    logger.debug('Triggered with hotword: **' + voice_command + '**')
     # compare to phrase_assistant from config
     voice_command_assistant = config['triggers']['pocketsphinx']['phrase_assistant']
-    if voice_command in voice_command_assistant:
+    logger.debug('Google Assistant Hotword(s): ' + str(voice_command_assistant))
+    if voice_command in voice_command_assistant: # works for both lists and strings
         global p
         if p is not None:
             # SDK is ready, start recording
@@ -553,6 +561,12 @@ def process_response(response):
                         player.set_volume(volume)
 
                         logger.debug("new volume = %s", volume)
+
+                        ### Resume playing the last station after setting volume
+                        if player.last_playlist is not None:
+                            player.play_playlist(player.last_playlist)
+                        ###
+
 
         # Additional Audio Iten
         elif 'audioItem' in j['messageBody']:
